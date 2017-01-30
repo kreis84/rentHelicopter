93ae2709a677bc898e6ffb44031ce75e83f4fd0a
Krzysztof Mazurkiewicz <k3mazurkiewicz@gmail.com>
2017-01-30 17:55:47 +0100

    home ready

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
 $(document).ready(
-	function(){
+function(){
+	APP.init();
+});
 
-	});
+var APP = (function()
+{
+	let elementToLoadTo = $('main');
+	function getSubpages(url)
+	{
+		$.ajax({url: url,
+				dataType: 'html'})
+			.done(function (result)
+			{
+				elementToLoadTo.html(result);
+			})
+			.fail(function(error)
+			{
+				elementToLoadTo.text(error.statusText);
+			});
+
+	}
+
+	return{
+		init: function()
+		{
+			getSubpages('subpages/main.html');
+			$('nav li').on('click', function(){ 
+						getSubpages($(this).attr('data-url'));
+					});
+		}
+	}
+
+})();
