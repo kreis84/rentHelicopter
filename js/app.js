7120eb3256c82e6e89f1657cc97f2c8ef0c241a0
Krzysztof Mazurkiewicz <k3mazurkiewicz@gmail.com>
2017-01-31 12:29:35 +0100

    in construction part added

--- js/app.js
+++ js/app.js
@@ -8,30 +8,29 @@ var APP = (function()
 	let elementToLoadTo = $('main');
 	function getSubpages(url)
 	{
-		$.ajax({url: url,
+		elementToLoadTo.slideUp(400, function()
+		{
+			$.ajax({url: url,
 				dataType: 'html'})
-			.done(function (result)
-			{
-				elementToLoadTo.html(result);
-			})
-			.fail(function(error)
-			{
-				elementToLoadTo.text(error.statusText);
-			});
-
+				.done(function (result)
+				{
+					elementToLoadTo.html(result);
+					elementToLoadTo.slideDown();
+				})
+				.fail(function(error)
+				{
+					elementToLoadTo.text(error.statusText);
+				});
+		});
 	}
 
 	return{
 		init: function()
 		{
 			getSubpages('subpages/main.html');
-			$('nav li').on('click', function(){ 
-						getSubpages($(this).attr('data-url'));
-					});
-
-
-			getSubpages('subpages/rates.html');
-
+			$('nav li').each(function(index){
+				if(index < 3) $(this).on('click', function(){ getSubpages($(this).attr('data-url'));});
+			});
 		}
 	}
 
